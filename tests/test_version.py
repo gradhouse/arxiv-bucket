18ed07cdc7a71ebfdc9f8c9bd25d868d3e66d5a8
gradhouse <gradhouse@pm.me>
2025-09-14 11:06:01 +0200

    add versioning (#8)

---
+++ tests/test_version.py
@@ -0,0 +1,10 @@
+# File: test_version.py
+# Description: Unit tests for the versioning of the arxiv_bucket package.
+#
+# Copyright (c) 2025 Jason Stuber
+# Licensed under the MIT License. See the LICENSE file for more details.
+
+def test_version():
+    """Test that the package version is correctly set."""
+    import arxiv_bucket
+    assert arxiv_bucket.__version__ == "0.1.0"
